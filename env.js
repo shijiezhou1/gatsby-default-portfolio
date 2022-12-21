8f9c527f348bce18a9726e5fe3bf7b239d57a424
shijie <cs5129606@gmail.com>
2022-12-21 21:17:15 +0800

    Uploading basic config

---
+++ env.js
@@ -0,0 +1,8 @@
+// const IMAGE_UPLOAD = 'http://localhost:8080/api/images/uploadstring';
+
+// const HOSTNAME = 'http://localhost:8080';
+const HOSTNAME = 'https://api.shijiezhou.com';
+
+export const IMAGE_UPLOAD = `${HOSTNAME}/api/images/uploadstring`;
+export const IMAGE_LIST = `${HOSTNAME}/api/images`;
+export const IMAGE_DELETE = `${HOSTNAME}/api/images`;
